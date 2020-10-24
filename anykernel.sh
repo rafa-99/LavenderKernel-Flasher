dd283e3afb8ede28f137e64f742f4920e15fed07
rafa_99 <rafa99@protonmail.com>
2020-10-24 02:47:13 +0100

    Updated Scripts

--- anykernel.sh
+++ anykernel.sh
@@ -1,30 +1,40 @@
 # AnyKernel3 Ramdisk Mod Script
 # osm0sis @ xda-developers
 
+## AnyKernel setup
+# begin properties
 properties() { '
-kernel.string=Lavender Custom Kernel
+kernel.string=Lavender Custom Kernel for LineageOS
 do.devicecheck=1
 do.modules=0
-do.systemless=1
 do.cleanup=1
-do.cleanuponabort=1
-device.name1=
-device.name2=
-supported.versions=
+do.cleanuponabort=0
+device.name1=lavender
+supported.versions=9 - 10
 supported.patchlevels=
-'; }
+'; } # end properties
 
 # shell variables
 block=/dev/block/bootdevice/by-name/boot;
 is_slot_device=0;
 ramdisk_compression=auto;
 
+
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
 . tools/ak3-core.sh;
 
+
+## AnyKernel file attributes
+
+
 ## AnyKernel install
 dump_boot;
-write_boot;
 
-## end installrite_boot;
+# begin ramdisk changes
+
+
+# end ramdisk changes
+
+write_boot;
+## end install
